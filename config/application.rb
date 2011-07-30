6c849db68562bad1e10d4b6c647d7c80ea6daefc
ankit gupta <ankit.gupta8898@gmail.com>
2011-07-31 00:05:33 +0530

    changed the application rb to test heroku prod mail sending

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
-
+config.action_mailer.default_url_options = { :host => 'stop-kharcha.heroku.com' }
 module HomeManagement
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
